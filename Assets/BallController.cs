bd89c3b1d68c43d49c8d56a6dce6418909b397be
KengoOzawa <bc.ozawa@gmail.com>
2018-02-14 00:16:03 +0900

    Lesson5

---
+++ Assets/BallController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+
+    // ボールが見える可能性があるZ軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    // ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+
+    // 宣言(加筆部分)
+    GameObject director;
+
+	// Use this for initialization
+	void Start () {
+
+        //ScoreDirectorを取得 (加筆部分)
+        this.director = GameObject.Find("ScoreDirector");
+
+        // シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+	}
+	
+    // それぞれのオブジェクトとぶつかったとき対応する関数を呼び出す(加筆部分)
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "SmallStarPrefab")
+        {
+            this.director.GetComponent<ScoreDirector>().SmallStarPrefab();
+        }
+        else if (other.gameObject.tag == "largeStarPrefab")
+        {
+            this.director.GetComponent<ScoreDirector>().LargeStarPrefab();
+        }
+        else if (other.gameObject.tag == "SmallCloudPrefab")
+        {
+            this.director.GetComponent<ScoreDirector>().SmallCloudPrefab();
+        }
+        else if (other.gameObject.tag == "LargeCloudPrefab")
+        {
+            this.director.GetComponent<ScoreDirector>().LargeCloudPrefab();
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        // ボールが画面外に出た場合
+        if (this.transform.position.z < this.visiblePosZ) {
+            // GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+		
+	}
+}
---
+++ Assets/ScoreDirector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDirector : MonoBehaviour {
+
+    GameObject scorePoint;
+    int point = 0;
+
+    public void SmallStarPrefab() {
+        this.point += 10;
+    }
+    public void LargeStarPrefab(){
+        this.point += 50;
+    }
+    public void SmallCloudPrefab(){
+        this.point += 30;
+    }
+    public void LargeCloudPrefab(){
+        this.point += 100;
+    }
+
+	// Use this for initialization
+	void Start () {
+        this.scorePoint = GameObject.Find("ScoreText");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.scorePoint.GetComponent<Text>().text =
+                "Score : " + this.point.ToString();
+	}
+}
